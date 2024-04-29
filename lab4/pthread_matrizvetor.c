417d122d5b7bdf94898cf867ef82c6889005e949
Cloud User <ec2-user@ip-172-31-16-86.ec2.internal>
2024-04-29 20:12:39 +0000

    versão final e comentada do exercicio

--- lab4/pthread_matrizvetor.c
+++ lab4/pthread_matrizvetor.c
@@ -2,109 +2,127 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+// tipo de dado "thread_data" para armazenar dados enviados para threads
 struct thread_data {
-  int *linha_matriz;
-  int *vetor;
-  int *resultado;
-  int colunas;
-  int indice;
+    int *linha_matriz;
+    int *vetor;
+    int *resultado;
+    int colunas;
+    int indice;
 };
 
+// alocar memoria dinamicamente para matriz
 int** alocar_matriz(int linhas, int colunas){
-  int** matriz;
-  matriz = (int**)malloc(sizeof(int*)*linhas);
-  for (int i = 0; i < linhas; i++) {
-    matriz[i] = (int*)malloc(sizeof(int)*colunas);
-  }
-  return matriz;
-}
-
-void inserir_valores(int** matriz, int* vetor, int linhas, int colunas){
-  int num;
-  printf("\nInsira os valores para os elementos da matriz:\n");
-  for (int i = 0; i < linhas; i++){
-    for (int j = 0; j < colunas; j++){
-      printf("matriz[%d][%d]:", i, j);
-      scanf("%d",&num);
-      matriz[i][j] = num;
+    int** matriz;
+    // alocar memoria para linhas da matriz (vetores de inteiros)
+    matriz = (int**)malloc(sizeof(int*)*linhas);
+    for (int i = 0; i < linhas; i++){
+        // alocar memoria para colunas da matriz (inteiros)
+        matriz[i] = (int*)malloc(sizeof(int)*colunas);
     }
-  }
-
-  printf("\nInsira os valores para os elementos do vetor:\n");
-  for (int i = 0; i < colunas; i++){
-    printf("vetor[%d]:", i);
-    scanf("%d",&num);
-    vetor[i] = num;
-  }
+    return matriz;
 }
 
+// funcao para inserir valores para os elementos da matriz e do vetor
+void inserir_valores(int** matriz, int* vetor, int linhas, int colunas){
+    int num;
+
+    printf("\nInsira os valores para os elementos da matriz:\n");
+    for (int i = 0; i < linhas; i++){
+        for (int j = 0; j < colunas; j++){
+            printf("matriz[%d][%d]:", i, j);
+            scanf("%d",&num);
+            matriz[i][j] = num;
+        }
+    }
+
+    printf("\nInsira os valores para os elementos do vetor:\n");
+    for (int i = 0; i < colunas; i++){
+        printf("vetor[%d]:", i);
+        scanf("%d",&num);
+        vetor[i] = num;
+    }
+}
+
+// funcao que a thread executara
 void *thread_func(void *arg){
-  struct thread_data *data = (struct thread_data *)arg;
+    struct thread_data *data = (struct thread_data *)arg;
 
-  for (int i = 0; i < data->colunas; i++){
-    data->resultado[data->indice] += data->linha_matriz[i]*data->vetor[i];
-    //printf("data: %d\n",data->resultado[data->indice]);
-  }
+    // calculo do elementos[data->indice] do vetor resultado
+    for (int i = 0; i < data->colunas; i++){
+        data->resultado[data->indice] += data->linha_matriz[i]*data->vetor[i];
+    }
 
-  return NULL;
+    return NULL;
 }
 
 int main(void){
-  int linhas, colunas;
-  int **matriz;
-  int *vetor;
-  int *resultado;matriz = (int**)malloc(sizeof(int*)*linhas);
-for (int i = 0; i < linhas; i++) {
-  matriz[i] = (int*)malloc(sizeof(int)*colunas);
-}
+    // declaracao de veriaveis
+    int linhas, colunas;
+    int **matriz;
+    int *vetor;
+    int *resultado;
 
-  printf("Informe o numero de linhas da matriz: ");
-  scanf("%d",&linhas);
-  printf("Informe o numero de colunas da matriz: ");
-  scanf("%d",&colunas);
+    // leitura do input do usuario
+    printf("Informe o numero de linhas da matriz: ");
+    scanf("%d",&linhas);
+    printf("Informe o numero de colunas da matriz: ");
+    scanf("%d",&colunas);
 
-  matriz = alocar_matriz(linhas, colunas);
+    // alocar memoria dinamicamente para as variaveis: matriz, vetor e resultado
+    matriz = alocar_matriz(linhas, colunas);
 
-  vetor = (int*)malloc(sizeof(int)*colunas);
+    vetor = (int*)malloc(sizeof(int)*colunas);
 
-  resultado = (int*)malloc(sizeof(int)*linhas);
+    resultado = (int*)malloc(sizeof(int)*linhas);
 
-  inserir_valores(matriz, vetor, linhas, colunas);
+    // receber dados de input do usuario
+    inserir_valores(matriz, vetor, linhas, colunas);
 
-  pthread_t *threads;
-  threads = (pthread_t*)malloc(sizeof(pthread_t)*linhas);
+    // criar um vetor de threads (utilizando ponteiro)
+    pthread_t *threads;
+    // alocar memoria dinamicamente para o vetor de threads
+    threads = (pthread_t*)malloc(sizeof(pthread_t)*linhas);
 
-  struct thread_data *data;
-  data = (struct thread_data*)malloc(sizeof(struct thread_data)*linhas);
+    // criar um vetor do tipo de dado "thread_data"
+    struct thread_data *data;
+    // alocar memoria dinamicamente para o vetor de "thread_data", uma variavel "thread_data" para cada thread criada
+    data = (struct thread_data*)malloc(sizeof(struct thread_data)*linhas);
 
-  for (int i = 0; i < linhas; i++){
-    data[i].linha_matriz = matriz[i];
-    data[i].vetor = vetor;
-    data[i].resultado = resultado;
-    data[i].colunas = colunas;
-    data[i].indice = i;
-    pthread_create(&threads[i], NULL, thread_func, &data[i]);
-  }
-
-  for (int i = 0; i < linhas; i++){
-    pthread_join(threads[i], NULL);
-  }
-
-  printf("\nMatriz:\n");
-  for (int i = 0; i < linhas; i++) {
-    for (int j = 0; j < linhas; j++) {
-      printf("%d ", matriz[i][j]);
+    // criar threads, quantidade de threads = numero de linhas da matriz (uma thread para calcular cada elemento do vetor "resultado")
+    for (int i = 0; i < linhas; i++){
+        // atribuir valores para data[i], que sera utilizado pela threada[i]
+        data[i].linha_matriz = matriz[i];
+        data[i].vetor = vetor;
+        data[i].resultado = resultado;
+        data[i].colunas = colunas;
+        data[i].indice = i;
+        pthread_create(&threads[i], NULL, thread_func, &data[i]);
     }
-    printf("\n");
-  }
 
-  printf("\nVetor:\n");
-  for (int i = 0; i < linhas; i++) {
-    printf("%d\n", vetor[i]);
-  }
-  printf("\nResultado: \n");
+    // esperar todas as threads terminarem a execucao
+    for (int i = 0; i < linhas; i++){
+        pthread_join(threads[i], NULL);
+    }
+
+    // imprimir valores da matriz, vetor e resultado para o usuario
+    printf("\nMatriz:\n");
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\nVetor:\n");
+    for (int i = 0; i < colunas; i++) {
+        printf("%d\n", vetor[i]);
+    }
+
+    printf("\nResultado: \n");
     for (int j = 0; j < linhas; j++){
         printf("%d \n", resultado[j]);
     }
-  return 0;
+
+    return 0;
 }
