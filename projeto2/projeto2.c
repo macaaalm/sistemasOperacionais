d875ea8ba31f02348509926db24b46b1207edc54
Cloud User <ec2-user@ip-172-31-16-86.ec2.internal>
2024-05-28 02:24:02 +0000

    versao final de projeto2.c

--- projeto2/projeto2.c
+++ projeto2/projeto2.c
@@ -4,19 +4,24 @@
 #include <stdlib.h>
 #include <string.h>
 
+// criar mutex para acessar contas 1 e 2 (to, from)
 pthread_mutex_t lockc1;
 pthread_mutex_t lockc2;
 
+// tipo de dado struct (conta)
 struct conta {
   char nome[100];
-  int saldo; // adiciono um mutex a estrutura da conta
+  int saldo; 
 };
 
+// tipo de dado enviado para thread
 struct thread_data {
+
   struct conta *c1;
   struct conta *c2;
 };
 
+// valor utilizado na transferencia
 int valor;
 
 // a thread filha executara essa funcao
@@ -27,17 +32,18 @@ void *transferencia(void *arg) {
   pthread_mutex_lock(&lockc1);
   pthread_mutex_lock(&lockc2);
 
-  if (data->c1->saldo >=
-      valor) { // caso eu tiver saldo suficiente na conta 'from'...
-    data->c1->saldo -= valor; // aqui retiro o valor da conta 'from'
-    data->c2->saldo += valor; // e depois adiciono a conta 'to'
-    printf("Transferencia de %d reais da conta %s\n", valor, data->c1->nome);
-    printf("Transferencia de %d reais para a conta %s\n", valor,
-           data->c2->nome);
+  if (data->c1->saldo >= valor) { // caso eu tiver saldo suficiente na conta 'c1'...
+    data->c1->saldo -= valor; // aqui retiro o valor da conta 'c1'
+    data->c2->saldo += valor; // e depois adiciono a conta 'c2'
+    printf("Transferencia de %d reais da conta %s para a conta %s\n", valor, data->c1->nome, data->c2->nome);
+    // mensagem de transferencia realizada com sucesso
+    printf("Transferencia realizada com sucesso!\n");
+    printf("Saldo da conta %s: %d\n", data->c1->nome, data->c1->saldo);
+    printf("Saldo da conta %s: %d\n", data->c2->nome, data->c2->saldo);
+  } else{
+    printf("Saldo insuficiente na conta %s\n", data->c1->nome);
   }
-  printf("Transferencia realizada com sucesso!\n");
-  printf("Saldo da conta %s: %d\n", data->c1->nome, data->c1->saldo);
-  printf("Saldo da conta %s: %d\n", data->c2->nome, data->c2->saldo);
+  
   // desbloqueio as contas apos acessa-las
   pthread_mutex_unlock(&lockc1);
   pthread_mutex_unlock(&lockc2);
@@ -54,23 +60,26 @@ int main() {
   pthread_mutex_init(&lockc1, NULL);
   pthread_mutex_init(&lockc2, NULL);
 
+  // criar variaveis conta (from, to) com nomes (conta1, conta2)
   struct conta from, to;
   strcpy(from.nome, "conta1");
   strcpy(to.nome, "conta2");
+  
   // as contas comecam com saldo 100
   from.saldo = 100;
   to.saldo = 100;
+  
+  valor = 1; // valor da transferencia
 
-  printf("Transferindo 10 para a conta c2\n");
-  valor = 1;
-
-  struct thread_data *data =
-      (struct thread_data *)malloc(sizeof(struct thread_data) * 100);
+  // criar vetor de thread_data
+  struct thread_data *data = (struct thread_data *)malloc(sizeof(struct thread_data) * 100);
+  
   // criar um vetor de threads (utilizando ponteiro)
   pthread_t *threads;
   // alocar memoria dinamicamente para o vetor de threads
   threads = (pthread_t *)malloc(sizeof(pthread_t) * 100);
-
+  
+  // criar 100 threads
   for (i = 0; i < 100; i++) {
     if (i % 5 == 0) { // exemplificando como as contas podem trocar de ordem (20
                       // vezes nesse caso)
@@ -84,6 +93,7 @@ int main() {
     }
   }
 
+  // esperar threads terminarem a execucao
   for (i = 0; i < 100; i++) {
     pthread_join(threads[i], NULL);
   }
@@ -94,9 +104,9 @@ int main() {
 
   printf("Transferencias concluidas e memoria liberada.\n");
 
+  // liberar memoria
   free(data);
   free(threads);
 
   return 0;
 }
-
